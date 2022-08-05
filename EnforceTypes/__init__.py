e97d1ba636be0d98141486bc4be0e437671da4b4
Riven Skaye <pretender0906@gmail.com>
2022-08-05 17:51:22 +0200

    Some fixes, split classes and functions

--- EnforceTypes/__init__.py
+++ EnforceTypes/__init__.py
@@ -13,21 +13,39 @@ and it works for singular functions (wrapping any calls made to them).
 
 .. code-block:: python
 
-   from EnforceTypes import enforce_types
+   from EnforceTypes import classtypes, functypes
 
 
-   @enforce_types
+   @functypes
    def add(a: int, b: int) -> None
        print(f"Adding {a} to {b} equals {a + b}")
 
+
    add(2, 2)  # prints "Adding 2 to 2 equals 4"
    add("a", 2)  # This causes the decorator to raise a TypeError!
+
+   @classtypes
+   class A:
+       def __init__(a: int, b: int):
+           self.a = a
+           self.b = b
+
+       @property
+       calc():
+           print(f"Adding {self.a} to {self.b} equals {self.a + self.b}")
+
+
+    A(1, 1).calc  # prints 2
+    A(1, "b").calc  # This causes a TypeError too, before calling `A.calc`!
 """
-from typing import Any, Type, TypeVar, get_args
+from typing import Any, Type, TypeVar, get_args, Callable, Dict
+from functools import wraps
 
 __all__ = [
-    "enforce_types"
+    "classtypes", "functypes"
 ]
+
+__version__ = "0.0.1"
 
 
 class _MISSING:
@@ -36,15 +54,16 @@ class _MISSING:
 
 MISSING = _MISSING()
 T = TypeVar("T")
+classtype = type(_MISSING)
 
 
-def enforce_types(cls: Type[T]) -> Type[T]:
-    """A class for use as class decorator. Enforces types in ``init``"""
+def classtypes(cls: Type[T]) -> Type[T]:
+    """For use as class decorator. Enforces types in ``init``"""
 
-    def newinit(self: T, *args: Any, **kwargs: Any) -> None:
-        keywords = cls.__annotations__ or cls.__init__.__annotations__
-        oldinit = self.__dict__.get("__init__") if "__dict__" in dir(self) else None
-        oldinit = oldinit or (lambda *a, **k: None)
+    @wraps(cls)
+    def newinit(self: Type[T], *args: Any, **kwargs: Any) -> None:
+        keywords: Dict[str, Any] = self.dict.get("__init__").__annotations__  # type: ignore
+        oldinit: Callable = self.__dict__.get("__init__")  # type: ignore
         arglist = list(args)
         for kw in keywords:
             argval = arglist.pop(0) if len(arglist) > 0 else ...
@@ -62,7 +81,35 @@ def enforce_types(cls: Type[T]) -> Type[T]:
                 raise TypeError(f"Argument {kw} was passed a value of type `"
                                 f"{type(kw_val).__name__}`, but only accepts values of "
                                 f"type `{argtype.__name__}`")
-        oldinit(*args, **kwargs)
+        oldinit(*arglist, **kwargs)
 
     setattr(cls, "__init__", newinit)
     return cls
+
+
+def functypes(func: Callable) -> Callable:
+    """For use as a function decorator, enforces types in ``__call__``"""
+
+    @wraps(func)
+    def newcall(func: Callable, *args: Any, **kwargs: Any) -> T:
+        keywords = func.__annotations__
+        arglist = list(args)
+        for kw in keywords:
+            argval = arglist.pop(0) if len(arglist) > 0 else ...
+            kw_val = kwargs.get(kw, MISSING)
+            if argval is not ... and kw_val is not MISSING:
+                raise TypeError(f"{kw} was given as both a positional and a keyword argument!")
+            if kw_val is MISSING:
+                if argval is ...:
+                    continue
+                kw_val = argval
+                kwargs[kw] = kw_val
+            argtype = keywords[kw]
+            argtuple = get_args(argtype)
+            if not isinstance(kw_val, argtype) and not isinstance(kw_val, argtuple):
+                raise TypeError(f"Argument {kw} was passed a value of type `"
+                                f"{type(kw_val).__name__}`, but only accepts values of "
+                                f"type `{argtype.__name__}`")
+        return func(*arglist, **kwargs)
+
+    return newcall

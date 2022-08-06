8bcdc47f5a25c7ac66aaa8fcaa34737c1eea60af
Riven Skaye <pretender0906@gmail.com>
2022-08-06 19:12:00 +0200

    It works, don't break it again please

--- EnforceTypes/__init__.py
+++ EnforceTypes/__init__.py
@@ -1,45 +1,8 @@
-"""
-Simple decorators for enforcing types during runtime.
-
-The idea behind this is to reduce time spent developing and using packages
-that are computationally intensive and take a long time to run, which might
-still fail and error on type problems. The easiest way to resolve this is
-checking the types of all input arguments, which quickly becomes repetitive
-and does not aid in readability of the code base.
-
-This module provides simple decorators that handle checking of all input
-arguments upon being called, for both classes (wrapping __init__)
-and singular functions (wrapping any calls made to them).
-
-.. code-block:: python
-
-   from EnforceTypes import classtypes, functypes
-
-
-   @functypes
-   def add(a: int, b: int) -> None
-       print(f"Adding {a} to {b} equals {a + b}")
-
-
-   add(2, 2)  # prints "Adding 2 to 2 equals 4"
-   add("a", 2)  # This causes the decorator to raise a TypeError!
-
-   @classtypes
-   class A:
-       def __init__(a: int, b: int):
-           self.a = a
-           self.b = b
-
-       @property
-       calc():
-           print(f"Adding {self.a} to {self.b} equals {self.a + self.b}")
-
-
-    A(1, 1).calc  # prints 2
-    A(1, "b").calc  # This causes a TypeError too, before calling `A.calc`!
-"""
+"""Simple decorators for enforcing types during runtime."""
 from functools import wraps
-from typing import Any, Type, TypeVar, get_args, Callable, Dict
+from inspect import signature, _ParameterKind as PK
+from types import FunctionType
+from typing import Any, Callable, Type, TypeVar, cast, get_args
 
 __all__ = [
     "classtypes", "functypes"
@@ -54,60 +17,123 @@ class _MISSING:
 
 MISSING = _MISSING()
 T = TypeVar("T")
+F = TypeVar("F", bound=Callable)
 
 
-def classtypes(cls: Type[T]) -> Type[T]:
-    """For use as class decorator. Enforces types in ``init``"""
+def functypes(func: F) -> F:
+    """For use as a function decorator, enforces types in ``__call__``"""
+    assert func
 
-    def newinit(self: Type[T], *args: Any, **kwargs: Any) -> None:
-        keywords: Dict[str, Any] = self.dict.get("__init__").__annotations__  # type: ignore
-        oldinit: Callable = self.__dict__.get("__init__")  # type: ignore
+    @wraps(func)
+    def newcall(*args: Any, **kwargs: Any) -> Any:
+        keywords = signature(func).parameters
         arglist = list(args)
         for kw in keywords:
             argval = arglist.pop(0) if len(arglist) > 0 else ...
             kw_val = kwargs.get(kw, MISSING)
             if argval is not ... and kw_val is not MISSING:
-                raise TypeError(f"{kw} was given as both a positional and a keyword argument!")
+                raise SyntaxError(f"{kw} was given as both a positional and a keyword argument! "
+                                  f"({func.__name__})")
             if kw_val is MISSING:
                 if argval is ...:
                     continue
                 kw_val = argval
-                kwargs[kw] = kw_val
-            argtype = keywords[kw]
+                if keywords[kw].kind == PK.POSITIONAL_ONLY:
+                    arglist.append(kw_val)
+                else:
+                    if keywords[kw].kind == PK.KEYWORD_ONLY and kw not in kwargs:
+                        raise SyntaxError(f"{kw} is a keyword-only argument, but was given as "
+                                          f"positional argument! ({func.__name__})")
+                    kwargs[kw] = kw_val
+            argtype = keywords[kw].annotation
             argtuple = get_args(argtype)
             if not isinstance(kw_val, argtype) and not isinstance(kw_val, argtuple):
                 raise TypeError(f"Argument {kw} was passed a value of type `"
                                 f"{type(kw_val).__name__}`, but only accepts values of "
-                                f"type `{argtype.__name__}`")
-        oldinit(*arglist, **kwargs)
+                                f"type `{argtype.__name__}` "
+                                f"({func.__name__})")
+        return func(*arglist, **kwargs)
+
+    return cast(F, newcall)
+
+
+def classtypes(cls: Type[T]) -> Type[T]:
+    """For use as class decorator. Enforces types in ``init``"""
+    oldinit: Callable = getattr(cls, "__init__")  # type: ignore
+    # If this constructor is a built-in object or otherwise a wrapper around something
+    # then instantiation behavior is different
+    is_obj = not isinstance(oldinit, FunctionType)
+    keywords = signature(cls).parameters
+
+    def newinit(self: Type[T], *args: Any, **kwargs: Any) -> None:
+        arglist = list(args)
+        for kw in keywords:
+            argval = arglist.pop(0) if len(arglist) > 0 else ...
+            kw_val = kwargs.get(kw, MISSING)
+            if argval is not ... and kw_val is not MISSING:
+                raise TypeError(f"{kw} was given as both a positional and a keyword argument! "
+                                f"({cls.__name__})")
+            if kw_val is MISSING:
+                if argval is ...:
+                    continue
+                kw_val = argval
+                if keywords[kw].kind == PK.POSITIONAL_ONLY:
+                    arglist.append(kw_val)
+                else:
+                    if keywords[kw].kind == PK.KEYWORD_ONLY and kw not in kwargs:
+                        raise SyntaxError(f"{kw} is a keyword-only argument, but was given as "
+                                          f"positional argument! ({cls.__name__})")
+                    kwargs[kw] = kw_val
+            argtype = keywords[kw].annotation
+            argtuple = get_args(argtype)
+            if not isinstance(kw_val, argtype) and not isinstance(kw_val, argtuple):
+                raise TypeError(f"Argument {kw} was passed a value of type `"
+                                f"{type(kw_val).__name__}`, but only accepts values of "
+                                f"type `{argtype.__name__}` "
+                                f"({cls.__name__})")
+        if is_obj:
+            oldinit(self)
+        else:
+            oldinit(self, **kwargs)
 
     setattr(cls, "__init__", newinit)
     return cls
 
 
-def functypes(func: Callable) -> Callable:
-    """For use as a function decorator, enforces types in ``__call__``"""
+def methtypes(meth: F) -> F:
+    """For use as a decorator for methods and @classmethods where ``self`` or ``cls`` is passed"""
+    assert meth
 
-    @wraps(func)
-    def newcall(*args: Any, **kwargs: Any) -> T:
-        keywords = func.__annotations__
-        print(keywords)
+    @wraps(meth)
+    def newcall(selfcls: Type[T], *args: Any, **kwargs: Any) -> Any:
+        keywords = signature(meth).parameters
         arglist = list(args)
         for kw in keywords:
+            if kw in ["self", "cls"]:
+                continue
             argval = arglist.pop(0) if len(arglist) > 0 else ...
             kw_val = kwargs.get(kw, MISSING)
             if argval is not ... and kw_val is not MISSING:
-                raise TypeError(f"{kw} was given as both a positional and a keyword argument!")
+                raise SyntaxError(f"{kw} was given as both a positional and a keyword argument! "
+                                  f"({meth.__name__})")
             if kw_val is MISSING:
                 if argval is ...:
                     continue
                 kw_val = argval
-                kwargs[kw] = kw_val
-            argtype = keywords[kw]
+                if keywords[kw].kind == PK.POSITIONAL_ONLY:
+                    arglist.append(kw_val)
+                else:
+                    if keywords[kw].kind == PK.KEYWORD_ONLY and kw not in kwargs:
+                        raise SyntaxError(f"{kw} is a keyword-only argument, but was given as "
+                                          f"positional argument! ({meth.__name__})")
+                    kwargs[kw] = kw_val
+            argtype = keywords[kw].annotation
             argtuple = get_args(argtype)
             if not isinstance(kw_val, argtype) and not isinstance(kw_val, argtuple):
                 raise TypeError(f"Argument {kw} was passed a value of type `"
                                 f"{type(kw_val).__name__}`, but only accepts values of "
-                                f"type `{argtype.__name__}`")
-        return func(*arglist, **kwargs)
-    return newcall
+                                f"type `{argtype.__name__}` "
+                                f"({meth.__name__})")
+        return meth(selfcls, *arglist, **kwargs)
+
+    return cast(F, newcall)

1c0ffeee17f8396ca3dcd3f252319aca18a1f38d
Riven Skaye <riven@tae.moe>
2022-11-17 16:29:36 +0100

    staticmethod works

--- EnforceTypes/Combined.py
+++ EnforceTypes/Combined.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass as dc
-from typing import Type
+from typing import Type, cast
 
-from .types import T
-from .Classes import classtypes
+from .types import F, T
+from .Classes import classtypes, methtypes
+from.Functions import functypes
 
 __all__ = [
     "dataclass"
@@ -11,3 +12,7 @@ __all__ = [
 
 def dataclass(cls: Type[T]) -> Type[T]:
     return classtypes(dc(cls))
+
+
+def staticmeth(meth: F) -> F:
+    return cast(F, staticmethod(functypes(meth)))

1c0ffee81bb66e00b9946eda49e84ee127bb28c3
Riven Skaye <riven@tae.moe>
2022-11-22 13:58:47 +0100

    Handle `@deco()` and `@deco` properly

--- EnforceTypes/Combined.py
+++ EnforceTypes/Combined.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass as dc
-from typing import Type
+from functools import partial
+from typing import Callable, Optional, Type, cast
 
 from .Classes import classtypes
 from .types import T, cast_to_func
@@ -9,17 +10,14 @@ __all__ = [
 ]
 
 
-def _dataclass(cls: Type[T], **kwargs: bool) -> Type[T]:
+def _dataclass(cls: Optional[Type[T]] = None, **kwargs: bool) -> Callable[[Type[T]], Type[T]]:
     """
     Creates a type-safe dataclass, used just like @dataclasses.dataclass.
     """
-    return classtypes(dc(cls, **kwargs))
+    if cls is not None:
+        return classtypes(dc(cls, **kwargs))  # type: ignore
+    else:
+        return cast(Callable, partial(_dataclass, **kwargs))
 
 
 dataclass = cast_to_func(_dataclass, dc)
-dataclass.__doc__ = f"""
-Creates a type-safe version of a Dataclass. Used the same as ``@dataclasses.dataclass``
-
-    {dc.__doc__}
-"""
-print(dataclass.__doc__)

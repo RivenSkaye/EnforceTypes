1c0ffee3bd9e69faca195b97a221e2472053df52
Riven Skaye <riven@tae.moe>
2022-11-25 15:53:53 +0100

    Bump version for the subscriptable fix

--- EnforceTypes/__init__.py
+++ EnforceTypes/__init__.py
@@ -4,7 +4,7 @@ from .Classes import classtypes, methtypes
 from .Combined import dataclass
 from .Functions import functypes
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 __all__ = [
     "classtypes", "dataclass", "functypes", "methtypes"
 ]
